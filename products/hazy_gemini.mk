8ca20420d2bd0c268141b856452c0f6e3be29f71
Salvatore Festa <valoxblood@gmail.com>
2016-08-04 10:55:08 +0200

    gemini: Updated to MIUI 6.7.24 dev configs

---
+++ products/hazy_gemini.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/xiaomi/gemini/full_gemini.mk)
+
+$(call inherit-product, vendor/hazy/main.mk)
+
+PRODUCT_NAME := hazy_gemini
+BOARD_VENDOR := Xiaomi
+TARGET_VENDOR := Xiaomi
+PRODUCT_DEVICE := gemini
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="gemini" \
+    PRODUCT_NAME="gemini" \
+    BUILD_FINGERPRINT="Xiaomi/gemini/gemini:6.0.1/MXB48T/6.7.24:user/release-keys" \
+    PRIVATE_BUILD_DESC="gemini-user 6.0.1 MXB48T 6.7.24 release-keys"
